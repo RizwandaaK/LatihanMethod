442a9ab0c4221bcb445cd8f21ac20d7771b42585
RizwandaaK <rizwanda_keysha_29rpl@student.smktelkom-mlg.sch.id>
2021-01-21 23:06:20 +0700

    LatihanMethod

---
+++ src/Method/Main.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Method;
+
+/**
+ *
+ * @author rizwa
+ */
+public class Main {
+    public static void main(String []args){
+        Void hewan = new Void();
+        hewan.KelompokHewan();
+        hewan.GerakHewani();
+    }
+    
+}
